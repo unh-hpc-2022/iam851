e0d45f10f73f88fc1c171ce74975e7f87b828480
Kai Germaschewski <kai.germaschewski@unh.edu>
2022-02-15 12:22:43 -0500

    class8: add bowlingScore() function
    So now the code at least compiles, though I made it so it still fails.

--- class8/bowling.cxx
+++ class8/bowling.cxx
@@ -3,6 +3,15 @@
 #include <vector>
 
 // ======================================================================
+// bowlingScore
+//
+// (normally one would want to separate the implemenation on of the
+// function / class from its tests, but to start its easiest to have it
+// all together.
+
+int bowlingScore(const std::vector<int> &rolls) { return -1; }
+
+// ======================================================================
 // Tests
 
 TEST(Bowling, AllZeros) {
