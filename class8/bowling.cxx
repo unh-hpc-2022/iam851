3a916d94746a1c015421369f171958adffacf576
Kai Germaschewski <kai.germaschewski@unh.edu>
2022-02-15 12:21:25 -0500

    class8: add first test for bowlingScore()
    It fails to even compile, since we don't have a bowlingScore() function yet.

--- class8/bowling.cxx
+++ class8/bowling.cxx
@@ -1,1 +1,13 @@
 #include <gtest/gtest.h>
+
+#include <vector>
+
+// ======================================================================
+// Tests
+
+TEST(Bowling, AllZeros) {
+  std::vector<int> rolls = {
+      0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
+  };
+  EXPECT_EQ(bowlingScore(rolls), 0);
+}
