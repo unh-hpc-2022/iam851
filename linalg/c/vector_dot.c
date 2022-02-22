3d5f530f010e52feb6b4e562c9f852ecc901afdc
Kai Germaschewski <kai.germaschewski@unh.edu>
2022-02-22 12:58:51 -0500

    class9: introduce VEC macro to access vector values
    This is may be a little bit prettier than having to access
    stuff through the data member directly, though beauty is in the eye of
    the beholder...

--- linalg/c/vector_dot.c
+++ linalg/c/vector_dot.c
@@ -15,7 +15,7 @@ double vector_dot(const struct vector* x, const struct vector* y)
   assert(x->n == y->n);
   double sum = 0.f;
   for (int i = 0; i < x->n; i++) {
-    sum += x->data[i] * y->data[i];
+    sum += VEC(x, i) * VEC(y, i);
   }
   return sum;
 }
