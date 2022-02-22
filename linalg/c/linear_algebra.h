7824c66a2059d66aa72d4563381de9077a21f0c3
Kai Germaschewski <kai.germaschewski@unh.edu>
2022-02-21 22:12:05 -0500

    class8s: add some const correctness to the linalg functions

--- linalg/c/linear_algebra.h
+++ linalg/c/linear_algebra.h
@@ -5,8 +5,8 @@
 // vector length (fixed)
 #define N (3)
 
-double vector_dot(double* x, double* y);
-void vector_add(double* x, double* y, double *z);
-void matrix_vector_mul(double A[N][N], double* x, double *y);
+double vector_dot(const double* x, const double* y);
+void vector_add(const double* x, const double* y, double *z);
+void matrix_vector_mul(const double A[N][N], const double* x, double *y);
 
 #endif
