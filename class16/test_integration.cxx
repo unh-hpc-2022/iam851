0b466b4c7e9a1514935e218a2a5d47f893014826
Kai Germaschewski <kai.germaschewski@unh.edu>
2022-03-24 12:55:45 +0000

    class16s: add timing to test_integration

--- class16/test_integration.cxx
+++ class16/test_integration.cxx
@@ -1,4 +1,6 @@
 
+#include "wtime.h"
+
 #include <cmath>
 #include <iostream>
 
@@ -9,13 +11,16 @@ double f(double x)
 
 int main(int argc, char** argv)
 {
-  const int N = 10;
+  const int N = 100000000;
 
+  double t1 = Wtime();
   double sum = 0.;
   double dx = 1. / N;
   for (int i = 0; i < N; i++) {
     sum += .5 * dx * (f(i * dx) + f((i + 1) * dx));
   }
+  double t2 = Wtime();
+  std::cout << "time " << t2 - t1 << "\n";
 
   std::cout << "integral is approximately " << sum << "\n";
 
