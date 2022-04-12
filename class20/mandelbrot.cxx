3149940f5e8fe01ec24538c16b293f30f6d68482
Kai Germaschewski <kai.germaschewski@unh.edu>
2022-04-11 20:54:46 -0400

    class20s: parallelize mandelbrot and time it

--- class20/mandelbrot.cxx
+++ class20/mandelbrot.cxx
@@ -1,4 +1,6 @@
 
+#include "wtime.h"
+
 #include <xtensor/xtensor.hpp>
 
 #include <complex>
@@ -23,8 +25,8 @@ int main(int argc, char** argv)
 {
   using namespace std::complex_literals;
 
-  const int MX = 400;
-  const int MY = 400;
+  const int MX = 2000;
+  const int MY = 2000;
 
   const std::complex<double> z0 = -2. - 1.i;
   const std::complex<double> z1 = 1. + 1.i;
@@ -34,13 +36,18 @@ int main(int argc, char** argv)
   double dx = std::real(z1 - z0) / (MX - 1);
   double dy = std::imag(z1 - z0) / (MY - 1);
 
+  double t1 = Wtime();
+#pragma omp parallel for
   for (int iy = 0; iy < MY; iy++) {
     for (int ix = 0; ix < MX; ix++) {
       std::complex<double> c = z0 + dx * ix + dy * iy * 1i;
       data(ix, iy) = calc_pixel(c);
     }
   }
+  double t2 = Wtime();
+  std::cout << "took " << t2 - t1 << " secs\n";
 
+#if 0
   FILE* file = fopen("mandelbrot.asc", "w");
   for (int iy = 0; iy < MY; iy++) {
     for (int ix = 0; ix < MX; ix++) {
@@ -50,4 +57,5 @@ int main(int argc, char** argv)
     fprintf(file, "\n");
   }
   fclose(file);
+#endif
 }
