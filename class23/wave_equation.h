85b7a234b2d7f04e51457e151cc49a10c90c5567
Kai Germaschewski <kai.germaschewski@unh.edu>
2022-04-20 23:09:10 -0400

    class24x: handle domain length and grid spacing in mpi_domain

--- class23/wave_equation.h
+++ class23/wave_equation.h
@@ -12,7 +12,7 @@ xt::xtensor<double, 1> derivative(const xt::xtensor<double, 1>& f, double dx);
 class mpi_domain
 {
 public:
-  mpi_domain(MPI_Comm comm, int N) : comm_(comm), N_(N)
+  mpi_domain(MPI_Comm comm, int N, double L) : comm_(comm), N_(N), L_(L)
   {
     MPI_Comm_rank(comm, &rank_);
     MPI_Comm_size(comm, &size_);
@@ -20,6 +20,7 @@ public:
     // (this could be relaxed, but needs to be handled properly)
     assert(N_ % size_ == 0);
     n_ = N_ / size_;
+    dx_ = L_ / N_;
   }
 
   MPI_Comm comm() const { return comm_; }
@@ -27,6 +28,7 @@ public:
   int size() const { return size_; }
   int N() const { return N_; }
   int n() const { return n_; }
+  double dx() const { return dx_; }
 
 private:
   MPI_Comm comm_;
@@ -34,6 +36,8 @@ private:
   int size_;
   int N_; // global number of grid points
   int n_; // local number of grid points (on this proc)
+  double L_;
+  double dx_;
 };
 
 #endif
