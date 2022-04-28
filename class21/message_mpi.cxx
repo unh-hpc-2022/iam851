ca366a69b466f77f37e7b7f95e771f5cac8ca33e
Kai Germaschewski <kai.germaschewski@unh.edu>
2022-04-27 22:48:18 -0400

    class21s: do a pair of MPI_Send/Recv

--- class21/message_mpi.cxx
+++ class21/message_mpi.cxx
@@ -12,6 +12,9 @@ int main(int argc, char** argv)
   int test = 0;
   if (rank == 0) {
     test = 99;
+    MPI_Send(&test, 1, MPI_INT, 3, 123, MPI_COMM_WORLD);
+  } else if (rank == 3) {
+    MPI_Recv(&test, 1, MPI_INT, 0, 123, MPI_COMM_WORLD, MPI_STATUS_IGNORE);
   }
 
   printf("Hello %d/%d test = %d\n", rank, size, test);
